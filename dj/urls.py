f67ced3218d1bcc86c9e486ec7f24f84c1c3fd43
chen jian ping <287487965@qq.com>
2014-03-10 18:01:55 +0800

    modify urls

--- dj/urls.py
+++ dj/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import *
 
 urlpatterns = patterns('dj.views',
     url(r'(?P<year>\d{4})/$', 'home', name='home'),
--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('news.views',
     url(r'^$', 'index', name='index'),
