799c8c61720aade8153dc2594f48af09d0dd1c8e
chen jian ping <287487965@qq.com>
2014-03-14 18:07:06 +0800

    add sql example

--- dj/views.py
+++ dj/views.py
@@ -9,6 +9,8 @@ from django.template.loader import get_template
 from django.utils import simplejson
 from dj.models import Publisher
 
+import MySQLdb
+ 
 def index(request):
   return HttpResponse('aaaa')
 
@@ -35,3 +37,16 @@ def test(request):
   li['price'] = ('12', '2-13-12-12')
   # return HttpResponse(simplejson.dumps(li), mimetype="application/json")
   # return HttpResponse(request.META['REMOTE_ADDR'])
+
+def db(request):
+    try:
+        conn = MySQLdb.connect(host='127.0.0.1', user='root', passwd='111111', port=3306) #, db='blog')
+        cur = conn.cursor()
+        conn.select_db('blog')
+        cur.execute('insert into blog_user(`name`, `age`, `addr`) values(%s, %s, %s)', ('chen ping2', 23, 'Bei Jing Caho Yang'))
+        conn.commit()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error,e:
+        print e
+    return HttpResponse('index')
